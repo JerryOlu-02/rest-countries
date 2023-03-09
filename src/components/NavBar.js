47225b1761076290baf0f9ed270a049c26b96492
JerryOlu-02 <joluwole20@gmail.com>
2023-03-09 19:53:46 +0100

    New Styles

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,6 @@
 import { useContext } from 'react';
 import CountryContext from '../context/countryContext';
-import { RiMoonLine } from 'react-icons/ri';
+import { RiMoonLine, RiMoonFill } from 'react-icons/ri';
 
 const NavBar = function () {
   const { toggleDarkMode, darkMode } = useContext(CountryContext);
@@ -16,7 +16,7 @@ const NavBar = function () {
       <h2>Where in the world?</h2>
 
       <button onClick={handlClick}>
-        <RiMoonLine />
+        {darkMode ? <RiMoonFill /> : <RiMoonLine />}
         <p>Dark Mode</p>
       </button>
     </div>
--- src/components/RenderCountryCard.js
+++ src/components/RenderCountryCard.js
@@ -4,7 +4,6 @@ import { useContext } from 'react';
 import CountryContext from '../context/countryContext';
 
 const RenderCountryCard = function ({ countryData }) {
-  console.log(countryData);
   const { darkMode } = useContext(CountryContext);
 
   const renderedButtons = countryData?.borders?.map((border) => {
